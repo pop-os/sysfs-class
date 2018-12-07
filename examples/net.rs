34a5365d026f87f3f46a814794a5d612a59215af
Michael Aaron Murphy <mmstickman@gmail.com>
2018-12-06 17:16:03 -0700

    :sparkles: /sys/class/net as Net

---
+++ examples/net.rs
@@ -0,0 +1,19 @@
+extern crate sysfs_class;
+use sysfs_class::{Net, SysClass};
+use std::io;
+
+fn main() -> io::Result<()> {
+    for dev in Net::iter() {
+        let dev = dev?;
+
+        println!("{}: {}", dev.id(), dev.address().unwrap());
+        println!("    MTU: {}", dev.mtu().unwrap());
+        println!("    Duplex: {:?}", dev.duplex());
+
+        let statistics = dev.statistics();
+        println!("    RX: {} MiB", statistics.rx_bytes().unwrap() / (1024 * 1024));
+        println!("    TX: {} MiB", statistics.tx_bytes().unwrap() / (1024 * 1024));
+    }
+
+    Ok(())
+}
